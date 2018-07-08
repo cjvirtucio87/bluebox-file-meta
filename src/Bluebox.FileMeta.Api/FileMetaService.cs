f5c36095f686b877d4d7fbe23fd41eab297aa077
CJ Virtucio <cvirtucio@mitre.org>
2018-07-07 20:58:16 -0400

    added xml doc and proj refs

--- src/Bluebox.FileMeta.Api/FileMetaService.cs
+++ src/Bluebox.FileMeta.Api/FileMetaService.cs
@@ -7,6 +7,11 @@
     /// </summary>
     public interface FileMetaService
     {
-
+        /// <summary>
+        /// <para>
+        /// Create a journal record representing a file and its part list.
+        /// </para>
+        /// </summary>
+        void Create(Dto.File file);
     }
 }
