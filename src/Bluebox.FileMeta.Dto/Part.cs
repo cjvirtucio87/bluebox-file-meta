c8f8a78c46c66e93cf4f2cbd393481fd4f8ecea2
Christopher Virtucio <cjvirtucio@CJs-MacBook-Pro.local>
2018-08-12 11:30:07 -0400

    removed hash

--- src/Bluebox.FileMeta.Dto/Part.cs
+++ src/Bluebox.FileMeta.Dto/Part.cs
@@ -19,11 +19,5 @@
         /// </para>
         /// </summary>
         public int FileId { get; }
-        /// <summary>
-        /// <para>
-        /// The encrypted hash representing the file part's fully-qualified namespace.
-        /// </para>
-        /// </summary>
-        public string Hash { get; }
     }
 }
