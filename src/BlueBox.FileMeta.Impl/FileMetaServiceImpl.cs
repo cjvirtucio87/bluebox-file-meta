87d1319a4d9358cab1ef2cc031adcaa8a77b3be2
CJ Virtucio <cvirtucio@mitre.org>
2018-08-31 15:58:27 -0400

    added impl

---
+++ src/BlueBox.FileMeta.Impl/FileMetaServiceImpl.cs
@@ -0,0 +1,23 @@
+using BlueBox.FileMeta.Api;
+using BlueBox.FileMeta.Dto;
+
+namespace BlueBox.FileMeta.Impl
+{
+
+    /// <inheritxmldoc/>
+    public class FileMetaServiceImpl : IFileMetaService
+    {
+
+        /// <inheritxmldoc/>
+        public void CreateFileRecord(File file)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritxmldoc/>
+        public File GetFileRecord(int file)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
