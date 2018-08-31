10e7e7672156500672cea26fcc3c708f4e102fb9
CJ Virtucio <cvirtucio@mitre.org>
2018-08-31 13:55:17 -0400

    renamed csproj for Dto project

---
+++ src/BlueBox.FileMeta.Dto/File.cs
@@ -0,0 +1,24 @@
+﻿namespace BlueBox.FileMeta.Dto
+{
+    using System.Collections.Generic;
+    /// <summary>
+    /// <para>
+    /// Representation of a file.
+    /// </para>
+    /// </summary>
+    public class File
+    {
+        /// <summary>
+        /// <para>
+        /// Identifier for the file.
+        /// </para>
+        /// </summary>
+        public int FileId { get; }
+        /// <summary>
+        /// <para>
+        /// Part list.
+        /// </para>
+        /// </summary>
+        public List<Part> Parts { get; }
+    }
+}
