fe1f134b7ff77d1dd384388b513d09e86d996495
Christopher Virtucio <cjvirtucio@CJs-MacBook-Pro.local>
2018-11-24 17:40:03 -0500

    creating and dropping counters table

--- src/BlueBox.FileMeta.Tests.Integration/sql/mysql_init.sql
+++ src/BlueBox.FileMeta.Tests.Integration/sql/mysql_init.sql
@@ -1,6 +1,11 @@
+DROP TABLE IF EXISTS counters;
 DROP TABLE IF EXISTS part;
 DROP TABLE IF EXISTS file;
 
+CREATE TABLE counters (
+    next_id INT NOT NULL,
+    table_name VARCHAR(2500) NOT NULL UNIQUE
+)
 CREATE TABLE file (
     Id INT PRIMARY KEY
 );
